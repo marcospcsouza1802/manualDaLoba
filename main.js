0ee7b89fd60535e548a00c65360ea50dbbaae2ae
Marcos <marcospcsouza1802@gmail.com>
2023-08-29 21:47:41 -0300

    pitch compra

---
+++ main.js
@@ -0,0 +1,8 @@
+function showSubmit() {
+    document.getElementById('btn-vsl').style.display = 'block';
+    document.getElementById('btn-caminho').style.display = 'block';
+    document.getElementById('btn-depoimentos').style.display = 'block';
+    document.getElementById('tela6').style.display = 'block';
+}
+
+setTimeout(showSubmit, 180000);;
