4442bacffb9518df186402f7bdd7c1c3fa5600a0
Marcos <marcospcsouza1802@gmail.com>
2023-09-08 20:30:10 -0300

    pitch botao

---
+++ main.js
@@ -0,0 +1,8 @@
+function showSubmit() {
+    document.getElementById('tela6').style.display = 'block';
+    document.getElementById('btn-vsl').style.display = 'block';
+    document.getElementById('btn-caminho').style.display = 'block';
+    document.getElementById('btn-depoimentos').style.display = 'block';
+}
+
+setTimeout(showSubmit, 125000);;
