5dd0f26ce50782d0a88305ad3b2e8c5ff36486b8
Marcos <marcospcsouza1802@gmail.com>
2023-09-18 21:29:41 -0300

    mostrar preco

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-function showSubmit() {
+/* function showSubmit() {
     document.getElementById('tela6').style.display = 'block';
     document.getElementById('btn-vsl').style.display = 'block';
     document.getElementById('btn-caminho').style.display = 'block';
     document.getElementById('btn-depoimentos').style.display = 'block';
 }
 
-setTimeout(showSubmit, 125000);;
+setTimeout(showSubmit, 125000);*/
